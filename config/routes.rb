1dbcb950c782fef96c73fbc5cf00a1a16a65a1a7
vishnukulangara <vishnukulangara@qburst.com>
2016-09-09 11:35:49 +0530

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'devise/sessions#new'
+  root 'home#index'
   
   mount Resque::Server.new, at: "/resque"
   
